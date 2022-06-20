1b34db73ff552ddfcfac217fd40122befae1650b
lloydapd <97029307+lloydapd@users.noreply.github.com>
2022-06-20 01:19:44 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
+import { MostPlayed, LatestUpdates, BestFPS, NowStreaming, MustPlayGame, NintendoDirect, HappeningNow, Footer } from '../sections'
+
+export default function Home() {
+  return (
+    <Box>
+      <Head>
+        <title>Simple UI</title>
+        <meta name="description" content="Created by Lloyd Gabriel Llurag" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Kdam+Thmor+Pro&display=swap" rel="stylesheet"/>
+      </Head>
+      <MostPlayed/>
+      <LatestUpdates/>
+      <BestFPS/>
+      <NowStreaming/>
+      <MustPlayGame/>
+      <NintendoDirect/>
+      <HappeningNow />
+      <Footer/>
+    </Box>
+  )
+}
